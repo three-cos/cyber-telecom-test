df03ddc7a93e53f5b7c7ea22c3c6553879775014
Андрей Килязов <a.kilyazov@tapir.ws>
2024-06-06 15:39:38 +0300

    install dev-dependencies; add pre-commit hook

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ cp docker-compose.override.dist.yml docker-compose.override.yml
 
 make up
 ```
+
+### Install pre-commit hook
+```bash
+cp ./pre-commit .git/hooks/
+chmod +x .git/hooks/pre-commit
+```
--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -7,9 +7,28 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ *
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property mixed $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
+ * @method static \Database\Factories\UserFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @mixin \Eloquent
+ */
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
